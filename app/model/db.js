e29752fdf77442de82a0b524812de1891057c248
thitijermram <36046946+thitijermram@users.noreply.github.com>
2019-01-24 11:27:05 +0700

    Update db.js

--- app/model/db.js
+++ app/model/db.js
@@ -5,9 +5,9 @@ var mysql = require('mysql');
 //local mysql db connection
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'ventilator'
+    user     : 'id7769008_thiti7600',
+    password : 'dew28433',
+    database : 'id7769008_trackingventilator'
 });
 
 connection.connect(function(err) {
